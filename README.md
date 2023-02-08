8c5c03884a2cfaf60c53ba7b51155a01da48d039
Avinashtech-art <124765132+Avinashtech-art@users.noreply.github.com>
2023-02-08 12:15:05 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-lyb4uq
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-lyb4uq)
---
+++ index.js
@@ -0,0 +1,20 @@
+// Import stylesheets
+// import './style.css';
+const userFunction = (function () {
+   
+  return function () {
+      console.log(`Loading user details....`);
+     
+  }
+})();
+userFunction ()
+const fn2= ()=>'john wick'
+const name =fn2()
+console.log(name)
+
+
+
+//var firstName = 'Erik';
+// var lastName = 'Krieg';
+// var fullName = firstName + ' ' + lastName;  
+ 
